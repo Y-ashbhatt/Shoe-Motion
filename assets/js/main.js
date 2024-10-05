52bbd56305d040876b2a69bcb172640b2ff126c3
Yash Bhatt <0yashbhatt0@gmail.com>
2024-10-05 23:56:42 +0530

    Add Swiper

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,3 +28,29 @@ const linkAction = () =>{
     navMenu.classList.remove('show-menu');
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
+
+
+// SHOE SWIPER 
+
+const swiperShoe = new Swiper('.home__swiper', {
+    loop: true,
+    spaceBetween: 32,
+    grabCursor: true,
+    effect: 'creative',
+    creativeEffect: {
+        prev:{
+            translate: [-100, 0 , -500],
+            opacity: 0,
+        },
+        next:{
+            translate: [100, 0 , -500],
+            opacity: 0,
+        },
+    },
+
+    pagination: {
+      el: '.swiper-pagination',
+        clickable: true,
+    },
+  
+  });
